d96b4ebb7b2e2952f9f32c6a5779e8dfd8ed8ff9
Alvaro Avila <aavila@silkroadtech.com>
2018-12-03 07:15:12 -0400

    Added Common Actions

--- src/pages/base/base_page.py
+++ src/pages/base/base_page.py
@@ -3,9 +3,7 @@ from core.web_driver.driver_manager import DriverManager
 
 class BasePage:
 
-
-
     def __init__(self):
-        self.webDriver = DriverManager.get_instance().get_web_driver()
-        self.webDriverWait = DriverManager.get_instance().get_web_driver_wait()
-        PageFactory.initElements(webDriver, this);
+        self.web_driver = DriverManager.get_instance().get_web_driver().init_driver()
+        self.web_driver_wait = DriverManager.get_instance().get_web_driver_wait()
+        # PageFactory.initElements(self.web_driver, self)
--- src/pages/pivotal_page.py
+++ src/pages/pivotal_page.py
@@ -1,15 +1,23 @@
 from robot.api import logger
-from pages.login_page import LoginPage
+
+from pages.base.base_page import BasePage
+from pages.singin.login_page import LoginPage
+from utils.property_handler import PropertyHandler
 
 
-class PivotalPage:
+class PivotalPage(BasePage):
+
+    @FindBy
+    singin_btn = None
     def __init__(self):
         pass
 
     def click_login_btn(self):
         logger.info("PivotalPage::click login btn")
+        CommonActions.click_button()
         return LoginPage()
 
     def go_to_pivotal_page(self):
         logger.info("PivotalPage::go to pivotal page")
+        self.web_driver.get(PropertyHandler.get_instance().get_base_url())
 
